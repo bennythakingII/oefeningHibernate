f5d5aa63fb297ddb576f486dd785a8519fddeff0
Benamin Peeters <holyvengeance.wow@gmail.com>
2014-12-03 14:28:15 +0100

    Toegevoegd BO project
    taak: _  3/12/2014
    Bussines Object: Docent / Lokaal / Cursist / Module

---
+++ Code/BussinesObject/src/bussinesobject/BussinesObject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bussinesobject;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class BussinesObject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
